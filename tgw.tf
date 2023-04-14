4572124c3a1b57afb339843c25ea9441eaabd21c
Matt Kazmar <mkazmar@aviatrix.com>
2023-04-14 17:46:44 -0500

    almost enough

--- tgw.tf
+++ tgw.tf
@@ -1,5 +1,5 @@
 resource "aws_route" "rfc_10" {
-  count = var.use_tgw
+  count = var.use_tgw ? 1 : 0
 
   route_table_id         = aws_vpc.this.default_route_table_id
   destination_cidr_block = "10.0.0.0/8"
@@ -7,7 +7,7 @@ resource "aws_route" "rfc_10" {
 }
 
 resource "aws_route" "rfc_172" {
-  count = var.use_tgw
+  count = var.use_tgw ? 1 : 0
 
   route_table_id         = aws_vpc.this.default_route_table_id
   destination_cidr_block = "172.16.0.0/12"
@@ -15,7 +15,7 @@ resource "aws_route" "rfc_172" {
 }
 
 resource "aws_route" "rfc_192" {
-  count = var.use_tgw
+  count = var.use_tgw ? 1 : 0
 
   route_table_id         = aws_vpc.this.default_route_table_id
   destination_cidr_block = "192.168.0.0/16"
@@ -23,7 +23,7 @@ resource "aws_route" "rfc_192" {
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
-  count = var.use_tgw
+  count = var.use_tgw ? 1 : 0
 
   subnet_ids         = aws_subnet.tgw[*].id
   vpc_id             = aws_vpc.this.id
@@ -33,15 +33,14 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
   transit_gateway_default_route_table_propagation = false
 
   tags = merge({
-    Name           = var.name,
-    Network_Domain = var.network_domain
+    Name           = var.name
     },
     var.tags
   )
 }
 
 resource "aws_ec2_transit_gateway_route_table_association" "this" {
-  count = var.use_tgw && var.propogate ? 1 : 0
+  count = var.use_tgw  ? 1 : 0
 
   transit_gateway_attachment_id  = one(aws_ec2_transit_gateway_vpc_attachment.this).id
   transit_gateway_route_table_id = var.route_table_id
