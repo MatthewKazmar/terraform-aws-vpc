0597f2ae266c2a7df479790113cb4fb1d2925e06
Matt Kazmar <mkazmar@aviatrix.com>
2023-04-14 17:01:14 -0500

    output

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
-output "aws_vpc" {
+output "vpc" {
   description = "The AWS instance resource."
   value       = aws_vpc.this
 }
 
-output "aws_subnet_workload" {
+output "subnet_workload" {
   description = "The AWS Subnet resource for Workload."
   value       = aws_subnet.workload.this
 }
 
-output "aws_subnet_tgw" {
+output "subnet_tgw" {
   description = "The AWS Subnet resource for TGW."
   value       = aws_subnet.tgw.this
 }
 
-output "aws_security_group" {
+output "security_group" {
   description = "The AWS Security Group resource."
   value       = aws_security_group.this
 }
